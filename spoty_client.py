4a15b3044b60103f95d49292f2bc218645b22415
micheledinelli <dinellimichele00@gmail.com>
2023-02-04 12:48:57 +0100

    updated requirements

--- spoty_client.py
+++ spoty_client.py
@@ -1,4 +1,4 @@
-client_id=""
-client_secret=""
+client_id="a80867c5ce9640d4888f52ac2223df83"
+client_secret="b70ac654ad724ed3a96e25d6408edd96"
 redirect_uri="http://localhost:8081"
 scope="user-read-playback-state user-modify-playback-state user-library-read"
