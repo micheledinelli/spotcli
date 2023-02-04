e6e87066b35fcabf89ca6e658311932e75684413
micheledinelli <dinellimichele00@gmail.com>
2023-02-04 11:38:51 +0100

    added search feature

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from spotipy import util
 import os, re
 import spoty_client
 import inquirer
-from pprint import pprint
-import webbrowser
+import time
 
 console = Console()
 app = typer.Typer()
@@ -82,31 +81,81 @@ def whoami():
 def clear_cache():
     for f in os.listdir('./'):
         if re.search('^\.cache', f):
-            os.remove(f)      
+            os.remove(f)     
+
+@app.command(short_help="available devices")
+def devices(show: bool = typer.Option(False, help="show the available devices")):
+    token = isAuth()
+    sp = spotipy.Spotify(auth=token)
+    devices_dict = sp.devices()
+    device_list = devices_dict["devices"]
+    if show:
+        table = Table(show_header=True, header_style="bold blue")
+        table.add_column("id", min_width=25)
+        table.add_column("name", min_width=20, justify="center")
+        table.add_column("type", min_width=20)
+        table.add_column("is_active", min_width=20)
+        for idx, item in enumerate(device_list):
+            table.add_row(item["id"], item["name"], item["type"], str(item["is_active"]))
+        console.print(table)
+
+    return device_list
+
+def queue(device_id, uri):
+    token = isAuth()
+    sp = spotipy.Spotify(auth=token)
+    sp.add_to_queue(uri, device_id=None)
 
 @app.command()
 def search(q: str = typer.Argument("", help="query"), limit: int = typer.Option(5, help="number of results")):
     token = isAuth()
     sp = spotipy.Spotify(auth=token)
     results = sp.search(q=q, limit=limit)
-    choices = []
     songs_dict = results['tracks']
     song_items = songs_dict['items']
-    uris = []
+    name_uri_dict = {}
     for idx, item in enumerate(song_items):
-        choices.append(item['name'])
-        uris.append(item['external_urls']['spotify'])
+        name_uri_dict[item['name']] = item['external_urls']['spotify']
 
     questions = [
         inquirer.List(
             "tracks",
             message="Which song do you want to play?",
-            choices=choices,
+            choices=name_uri_dict.keys(),
         ),
     ]
-    answers = inquirer.prompt(questions)
-    pprint(answers)
-    webbrowser.open(uris[0])
+
+    answer = inquirer.prompt(questions)
+    track_name = answer['tracks']
+    device_list = devices(show=False)
+    for idx, device in enumerate(device_list):
+        if device["is_active"]:
+            active_id = device_list[0]['id']   
+            break
+    if active_id is None:
+        print("No active device found")
+
+    uri = name_uri_dict[track_name]
+    queue(device_id=active_id, uri=uri) 
+    sp.next_track(device_id=active_id)
+
+@app.command(short_help="stick to the queue")
+def stick():
+    token = isAuth()
+    sp = spotipy.Spotify(auth=token)
+    console.clear()
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("name", min_width=30, justify="center")
+    table.add_column("artist", min_width=30, justify="center")
+    table.add_column("album", min_width=30, justify="center")
+    current_track = sp.current_user_playing_track()
+    table.add_row(current_track["item"]["name"], current_track["item"]["artists"][0]["name"], current_track["item"]["album"]["name"], )
+    console.print(table)    
+    while True:
+        poll_track = sp.current_user_playing_track()
+        if current_track["item"]["id"] != poll_track["item"]["id"]:
+            stick()
+        time.sleep(3)
 
 if __name__ == "__main__":
     app()
