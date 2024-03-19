cd57b06a4913f1c319ecca5d02b38a44ac92edb6
micheledinelli <dinellimichele00@gmail.com>
2024-03-19 11:33:06 +0100

    added windows support

--- spotcli/utils.py
+++ spotcli/utils.py
@@ -4,4 +4,5 @@ CLIENT_ID="a80867c5ce9640d4888f52ac2223df83"
 CLIENT_SECRET="eca83a36fb8c4ebdbbc1ace8d6b7deb2"
 SCOPE="user-read-playback-state user-modify-playback-state user-library-read"
 REDIRECT_URI="http://localhost:8081"
-USERDATA_PATH = os.path.abspath(os.path.expanduser("~/.config/spoticli/userdata.json"))
+home_dir = os.path.expanduser("~")
+USERDATA_PATH = os.path.abspath(os.path.join(home_dir, ".config", "spoticli", "userdata.json"))
